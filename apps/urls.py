fe56c10d4b9059a1ab8962ec939f895d9855150a
narendravankayala <42788016+narendravankayala@users.noreply.github.com>
2019-05-09 15:28:37 -0400

    IGBF-1692-Fix categories issue with tags having spaces and special characters[-]

--- apps/urls.py
+++ apps/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path(r'',                                views.apps_default),
     re_path(r'all',                             views.all_apps,             name='all_apps'),
     path(r'wall',                            views.wall_of_apps,         name='wall_of_apps'),
-    re_path(r'with_tag/(\w{1,100})',            views.apps_with_tag,        name='tag_page'),
+    re_path(r'with_tag/(\w+ ?-?\w+)',            views.apps_with_tag,        name='tag_page'),
     re_path(r'with_author/(.{1,300})',          views.apps_with_author,     name='author_page'),
     re_path(r'^(?P<app_name>[-\w\d]+)',                     views.app_page,             name='app_page'),
     re_path(r'^(?P<app_name>[-\w\d]+)/edit',                views.app_page_edit,        name='app_page_edit'),
