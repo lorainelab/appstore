f86795d43199651424cd5297004b6c4bb2dbc516
Prutha Kulkarni <pruthakulkarni@Pruthas-MacBook-Air.local>
2019-06-18 12:08:33 -0400

    IGBF-1434 : Reove Cytoscape version 2 references from the codebase.

--- help/urls.py
+++ help/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path(r'about',                     views.about,      name='about'),
     path(r'contact',                   views.contact,    name='contact'),
-    path(r'competitions',              views.competitions,name='competitions'),
     path(r'md',                        views.md,         name='md'),
     path(r'getstarted',                views.getstarted, name='getstarted'),
     path(r'getstarted_app_install',    views.getstarted_app_install, name='getstarted_app_install'),
