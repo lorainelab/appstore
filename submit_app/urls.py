3881ceb2a16e98789c18ec399e5a0905f04b4bcc
aloraine <aloraine@gmail.com>
2019-09-04 12:31:59 -0400

    IGBF-1994: Remove javadocs, API (obsolete); delete previously commented code

--- submit_app/urls.py
+++ submit_app/urls.py
@@ -5,7 +5,5 @@ urlpatterns = [
     path(r'',                      views.submit_app,         name='submit-app'),
     path(r'pending',               views.pending_apps,       name='pending-apps'),
     re_path(r'confirm/(\d{1,5})',     views.confirm_submission, name='confirm-submission'),
-# IGBF-1994
-#    re_path(r'submit_api/(\d{1,5})',  views.submit_api,         name='submit-api'),
     path(r'artifact_exists',       views.artifact_exists),
 ]
