d04b33fa27b4de0ee7d9d8b48dbbedc4390941b3
Noor Zahara <nzahara@uncc.edu>
2019-10-17 15:02:22 -0400

    IGNF-2037 Change url structure - get help page

--- help/urls.py
+++ help/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path(r'about',                     views.about,      name='about'),
-    path(r'contact',                   views.contact,    name='contact'),
+    path(r'',                   views.contact,    name='contact'),
     path(r'md',                        views.md,         name='md'),
     path(r'getstarted',                views.getstarted, name='getstarted'),
     path(r'getstarted_app_install',    views.getstarted_app_install, name='getstarted_app_install'),
