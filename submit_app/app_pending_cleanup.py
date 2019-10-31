4976090a21b5b9bc0a58a51755dfc83506a9146b
Riddhi Patil <rpatil14@uncc.edu>
2019-10-31 08:46:46 -0700

    IGBF-2087 : Delete entries from app table which are 5 hours old

--- submit_app/app_pending_cleanup.py
+++ submit_app/app_pending_cleanup.py
@@ -2,9 +2,13 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 from util.view_util import get_object_or_none
 from .models import AppPending
+from datetime import datetime, timedelta
 
 def app_pending_cleanup():
-    not_submitter_approved_objs = AppPending.objects.filter(submitter_approved=False)
+    # delete only those entries which are more than 5 hours old
+    time_threshold = datetime.now() - timedelta(hours=5)
+    not_submitter_approved_objs = AppPending.objects.filter(submitter_approved=False, created__lte=time_threshold)
+    print(not_submitter_approved_objs.count())
     for not_submitter_approved_obj in not_submitter_approved_objs:
         submitter_approved_obj = get_object_or_none(AppPending, Bundle_SymbolicName=not_submitter_approved_obj.Bundle_SymbolicName, submitter_approved=True)
         if(submitter_approved_obj):
