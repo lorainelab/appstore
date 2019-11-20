b234de1bf7530f243eeffd4ddb1ae685e5e09439
Riddhi Patil <rpatil14@uncc.edu>
2019-11-20 12:27:20 -0800

    IGBF-2140 : Remove unused imports

--- submit_app/views.py
+++ submit_app/views.py
@@ -1,7 +1,6 @@
 import base64
 import re
 import os
-import datetime
 from os.path import basename
 from platform import release
 from urllib.request import urlopen
@@ -19,8 +18,6 @@ from util.id_util import fullname_to_name
 from util.view_util import html_response, json_response, get_object_or_none
 from .models import AppPending
 from .processjar import process_jar
-from download.models import ReleaseDownloadsByDate
-from django.db.models import F
 
 # IGBF-2026 start
 APP_REPLACEMENT_JAR_MSG = "This is a <b>replacement jar file</b> for a not yet released App that you or a colleague already uploaded previously but is still in our “pending apps” waiting area. If you choose to submit it, this new jar file will replace the one that was uploaded before."
