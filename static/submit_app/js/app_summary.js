749cb0d6d4ad39763ff627c5476dcb20e497259b
Philip Badzuh <philipbadzuh@gmail.com>
2019-12-20 10:10:06 -0500

    IGBF-2203 Update manifest info text on app submission confirmation page

--- static/submit_app/js/app_summary.js
+++ static/submit_app/js/app_summary.js
@@ -1,6 +1,6 @@
-var tooltip1 = "Bundle Name is a short, user-friendly name for your App. App Store and IGB App Manager displayBundle Name to users.See <a href='" + getDocLink() + "' target='_blank'>developer’s documentation</a>";
-var tooltip2 = "Bundle-SymbolicName and Bundle-Version uniquely identify an IGB App jar file within the IGB OSGi runtime. IGB needs Bundle-Version and Bundle-Symbolic Name to install and run your App. Users never see Bundle-SymbolicName, but IGB needs it to run your App. See <a href='" + getDocLink() + "' target='_blank'>developer’s documentation</a>";
-var tooltip3 = "Bundle-Version and Bundle-SymbolicName uniquely identify an IGB App jar file within the IGB OSGi runtime. IGB needs Bundle-Version and Bundle-Symbolic Name to install and run your App. Bundle-Version contain number and full-stop characters only, such as 0.0.1 or 6.1.0. See <a href='" + getDocLink() + "' target='_blank'>developer’s documentation</a>";
+var tooltip1 = "Bundle-Name is a short, user-friendly name for your App. Appstore and IGB App Manager display Bundle-Name to users. See <a href='" + getDocLink() + "' target='_blank'>developer’s documentation</a>.";
+var tooltip2 = "Bundle-SymbolicName identifies an IGB App jar file within the IGB OSGi runtime. Users never see Bundle-SymbolicName, but IGB needs it to install and run your App. See <a href='" + getDocLink() + "' target='_blank'>developer’s documentation</a>.";
+var tooltip3 = "Bundle-Version is also used to uniquely identify an IGB App jar file within the IGB OSGi runtime and is needed to install and run your App. Bundle-Version should contain only numeric and full-stop characters, e.g. 0.0.1 or 6.1.0. See <a href='" + getDocLink() + "' target='_blank'>developer’s documentation</a>.";
 
 document.getElementById("tooltip-1").title= tooltip1;
 document.getElementById("tooltip-2").title= tooltip2;
