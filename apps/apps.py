62f0fdd7cb4399ee523cba076ea5958c2df397a1
sshanbh1 <sshanbh1@uncc.edu>
2020-11-24 13:32:47 -0500

    IGBF-2655 : Implementing centralized Navbar for Appstore

--- apps/apps.py
+++ apps/apps.py
@@ -1,5 +1,20 @@
 from django.apps import AppConfig
-
+import requests
+import appstore.settings as settings
 
 class AppsConfig(AppConfig):
     name = 'apps'
+
+    def ready(self):
+
+        req_css = requests.get(settings.BIOVIZ_REPOSITORY + 'raw/' + settings.BIOVIZ_BRANCH + '/htdocs/css/menu.css')
+        req_html = requests.get(settings.BIOVIZ_REPOSITORY + 'raw/' + settings.BIOVIZ_BRANCH + '/htdocs/menu.html')
+
+        css_file = open('static/apps/css/menu.css', 'w')
+        html_file = open('templates/menu.html', 'w')
+
+        css_file.writelines(req_css.content.decode("utf-8"))
+        css_file.close()
+
+        html_file.writelines(req_html.content.decode("utf-8"))
+        html_file.close()
